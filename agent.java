d15b420959e7b0cb1236d5c29ab5d997342ad833
Keshar Wakharkar <59607058+ksh-r@users.noreply.github.com>
2021-02-20 19:34:28 +0530

    Updated agent

--- agent.java
+++ agent.java
@@ -3,8 +3,15 @@ import java.util.Queue;
 
 class agent {
 
-    public static void main(String args[]) {
+    static tileGame game;
     
+    public static void main(String args[]) {
+        game = new tileGame();
+        game.newDepthGame(15);
+        game.printCurrentState("Initial State :");
+        // char[][] startState = copyState();
+        BreadthFirstSearch();
+        game.printCurrentState("Final State :");
     }
 
 }
